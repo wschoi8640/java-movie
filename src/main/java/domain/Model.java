0b5ce1187de26cf4268f85731f8de0e2a1301a7e
wschoi8640 <cws911112@gmail.com>
2019-04-26 18:04:25 +0900

    ADD Model.java(Model Saving Data)

---
+++ src/main/java/domain/Model.java
@@ -0,0 +1,89 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Model {
+	 private Movie movie;
+	 private PlaySchedule schedule;
+	 private int ticketNum;
+	 private Ticket ticket;
+	 private int point = 0;
+	 private int creditOrCash;
+	 private List<Movie> movies;
+	 private List<PlaySchedule> schedules;
+	 private List<Ticket> cart = new ArrayList<Ticket>();
+	 
+	public List<Movie> getMovies() {
+		return movies;
+	}
+
+	public void setMovies(List<Movie> movies) {
+		this.movies = movies;
+	}
+
+	public List<PlaySchedule> getSchedules() {
+		return schedules;
+	}
+
+	public void setSchedules(List<PlaySchedule> schedules) {
+		this.schedules = schedules;
+	}
+
+	public PlaySchedule getSchedule() {
+		return schedule;
+	}
+
+	public void setSchedule(PlaySchedule schedule) {
+		this.schedule = schedule;
+	}
+
+	public int getTicketNum() {
+		return ticketNum;
+	}
+
+	public void setTicketNum(int ticketNum) {
+		this.ticketNum = ticketNum;
+	}
+
+	public Ticket getTicket() {
+		return ticket;
+	}
+
+	public void setTicket(Ticket ticket) {
+		this.ticket = ticket;
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+
+	public void addTicketsToCart() {
+		getCart().add(ticket);
+	}
+
+	public List<Ticket> getCart() {
+		return cart;
+	}
+
+	public int getPoint() {
+		return point;
+	}
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
+
+	public int getCreditOrCash() {
+		return creditOrCash;
+	}
+
+	public void setCreditOrCash(int creditOrCash) {
+		this.creditOrCash = creditOrCash;
+	}
+
+}
